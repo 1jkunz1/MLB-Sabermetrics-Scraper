d220e4aede4c3d13e12edb7e0c7c01a0e9858ce4
Joseph Kunzler <joe.kunzler@athenabitcoin.com>
2018-11-24 12:30:12 -0600

    utility functions

--- src/reddit_scraper/reddit_scraper.py
+++ src/reddit_scraper/reddit_scraper.py
@@ -3,25 +3,45 @@ import os
 import praw
 
 
+reddit = praw.Reddit(client_id='LsEMlrflEe17VA',
+                     client_secret='U5DIf2X4jFldQpsig2-w-S0nbNE',
+                     user_agent='sabermetric-scraper',
+                     username=os.getenv("USERNAME"),
+                     password=os.getenv("PASS"))
+
+
+def get_daily_threads(sport):
+
+    submissions = []
+
+    subreddit = reddit.subreddit('sportsbook')
+    new_subreddit = subreddit.new(limit=25)
+
+    for submission in new_subreddit:
+        submissions.append((submission.title, submission.id))
+
+    threads = dict(submissions)
+
+    for k, v in threads.items():
+        if sport not in k:
+            del threads[k]
+
+    return threads
+
+
 class RedditScraper(object):
 
     def __init__(self):
 
         self.comments = []
-        self.submissions = []
-        self.threads = {}
         self.submission = None
-
-        self.reddit = praw.Reddit(client_id='LsEMlrflEe17VA',
-                                  client_secret='U5DIf2X4jFldQpsig2-w-S0nbNE',
-                                  user_agent='sabermetric-scraper',
-                                  username= os.getenv("USERNAME"),
-                                  password=os.getenv("PASS"))
+        self.mlb_threads = None
+        self.nfl_threads = None
 
         self.subreddit = self.reddit.subreddit('sportsbook')
         self.new_subreddit = self.subreddit.new(limit=25)
 
-    def get_threads(self):
+    def get_daily_threads(self):
 
         # returns a dict with the top 25 most recent threads
         # we will run this once per day
@@ -31,7 +51,23 @@ class RedditScraper(object):
 
         self.threads = dict(self.submissions)
 
+        for k, v in self.threads.items():
+            if 'Daily' not in k:
+                del self.threads[k]
+
         return self.threads
+
+    @staticmethod
+    def get_nfl_daily(self):
+        return get_daily_threads('NFL')
+
+    @staticmethod
+    def get_mlb_daily(self):
+        return get_daily_threads('MLB')
+
+    @staticmethod
+    def get_ncaaf_daily(self):
+        return get_daily_threads('NCAAF')
 
     def make_comment_list(self, *kwargs):
 
